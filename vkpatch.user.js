0b37cc5e204d337f411971775a6627a9a3269c8d
pochemuto <devnull@localhost>
2010-01-18 12:01:13 +0000

    Заголовок скрипта

---
+++ vkpatch.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name           vkPatch
+// @namespace      http://klinifini.livejournal.com/
+// @description    Расширение функционала ВКонтакте.ру
+// @include        http://vkontakte.ru/*
+// ==/UserScript==
+// Author: Сергей Третьяк
+// Version: 6
+// Site: klinifini.livejournal.com
