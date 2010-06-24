21fc71a04e68e05ea663057817e128a580fba2b7
pochemuto <devnull@localhost>
2010-06-24 12:12:45 +0000

    Обновлен jQuery JSON Plugin до 2.2

--- vkpatch.user.js
+++ vkpatch.user.js
@@ -87,7 +87,8 @@ function init()
 		           boolTestParam2: vkPatch.settings.create().def(false).category('interface').done(),
 		           test2: vkPatch.settings.create().def(0).min(0).max(150).category('interface').done(),
 		           list: vkPatch.settings.create().def('two').list(['one','two','tree','more']).category('interface').done(),
-		           list2: vkPatch.settings.create().def('more5').list(['one1','two3','tree4','more5','exists','mgahah','test','ops','again']).category('myCat').done()
+		           list2: vkPatch.settings.create().def('more5').list(['one1','two3','tree4','more5','exists','mgahah','test','ops','again']).category('myCat').done(),
+		           obj: vkPatch.settings.create().def({}).done()
 		},
 		
 		lang:
@@ -413,20 +414,20 @@ var vkPatch =
 
 			/*
 			 * jQuery JSON Plugin
-			 * version: 1.0 (2008-04-17)
+			 * version: 2.1 (2009-08-14)
 			 *
 			 * This document is licensed as free software under the terms of the
 			 * MIT License: http://www.opensource.org/licenses/mit-license.php
 			 *
-			 * Brantley Harris technically wrote this plugin, but it is based somewhat
-			 * on the JSON.org website's http://www.json.org/json2.js, which proclaims:
+			 * Brantley Harris wrote this plugin. It is based somewhat on the JSON.org 
+			 * website's http://www.json.org/json2.js, which proclaims:
 			 * "NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.", a sentiment that
-			 * I uphold.  I really just cleaned it up.
+			 * I uphold.
 			 *
-			 * It is also based heavily on MochiKit's serializeJSON, which is 
-			 * copywrited 2005 by Bob Ippolito.
+			 * It is also influenced heavily by MochiKit's serializeJSON, which is 
+			 * copyrighted 2005 by Bob Ippolito.
 			 */
-			(function($){function toIntegersAtLease(n){return n<10?"0"+n:n}Date.prototype.toJSON=function(date){return this.getUTCFullYear()+"-"+toIntegersAtLease(this.getUTCMonth())+"-"+toIntegersAtLease(this.getUTCDate())};var escapeable=/["\\\x00-\x1f\x7f-\x9f]/g;var meta={"\b":"\\b","\t":"\\t","\n":"\\n","\f":"\\f","\r":"\\r",'"':'\\"',"\\":"\\\\"};$.quoteString=function(string){if(escapeable.test(string)){return'"'+string.replace(escapeable,function(a){var c=meta[a];if(typeof c==="string"){return c}c=a.charCodeAt();return"\\u00"+Math.floor(c/16).toString(16)+(c%16).toString(16)})+'"'}return'"'+string+'"'};$.toJSON=function(o,compact){var type=typeof(o);if(type=="undefined"){return"undefined"}else{if(type=="number"||type=="boolean"){return o+""}else{if(o===null){return"null"}}}if(type=="string"){return $.quoteString(o)}if(type=="object"&&typeof o.toJSON=="function"){return o.toJSON(compact)}if(type!="function"&&typeof(o.length)=="number"){var ret=[];for(var i=0;i<o.length;i++){ret.push($.toJSON(o[i],compact))}if(compact){return"["+ret.join(",")+"]"}else{return"["+ret.join(", ")+"]"}}if(type=="function"){throw new TypeError("Unable to convert object of type 'function' to json.")}var ret=[];for(var k in o){var name;type=typeof(k);if(type=="number"){name='"'+k+'"'}else{if(type=="string"){name=$.quoteString(k)}else{continue}}var val=$.toJSON(o[k],compact);if(typeof(val)!="string"){continue}if(compact){ret.push(name+":"+val)}else{ret.push(name+": "+val)}}return"{"+ret.join(", ")+"}"};$.compactJSON=function(o){return $.toJSON(o,true)};$.evalJSON=function(src){return eval("("+src+")")};$.secureEvalJSON=function(src){var filtered=src;filtered=filtered.replace(/\\["\\\/bfnrtu]/g,"@");filtered=filtered.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,"]");filtered=filtered.replace(/(?:^|:|,)(?:\s*\[)+/g,"");if(/^[\],:{}\s]*$/.test(filtered)){return eval("("+src+")")}else{throw new SyntaxError("Error parsing JSON, source is not valid.")}}})(jQuery);(function(){var a=false,b=/xyz/.test(function(){xyz})?/\b_super\b/:/.*/;this.Class=function(){};Class.extend=function(g){var f=this.prototype;a=true;var e=new this();a=false;for(var d in g){e[d]=typeof g[d]=="function"&&typeof f[d]=="function"&&b.test(g[d])?(function(h,i){return function(){var k=this._super;this._super=f[h];var j=i.apply(this,arguments);this._super=k;return j}})(d,g[d]):g[d]}function c(){if(!a&&this.init){this.init.apply(this,arguments)}}c.prototype=e;c.constructor=c;c.extend=arguments.callee;return c}})();
+			(function($){$.toJSON=function(o){if(typeof(JSON)=='object'&&JSON.stringify)return JSON.stringify(o);var type=typeof(o);if(o===null)return"null";if(type=="undefined")return undefined;if(type=="number"||type=="boolean")return o+"";if(type=="string")return $.quoteString(o);if(type=='object'){if(typeof o.toJSON=="function")return $.toJSON(o.toJSON());if(o.constructor===Date){var month=o.getUTCMonth()+1;if(month<10)month='0'+month;var day=o.getUTCDate();if(day<10)day='0'+day;var year=o.getUTCFullYear();var hours=o.getUTCHours();if(hours<10)hours='0'+hours;var minutes=o.getUTCMinutes();if(minutes<10)minutes='0'+minutes;var seconds=o.getUTCSeconds();if(seconds<10)seconds='0'+seconds;var milli=o.getUTCMilliseconds();if(milli<100)milli='0'+milli;if(milli<10)milli='0'+milli;return'"'+year+'-'+month+'-'+day+'T'+hours+':'+minutes+':'+seconds+'.'+milli+'Z"'}if(o.constructor===Array){var ret=[];for(var i=0;i<o.length;i++)ret.push($.toJSON(o[i])||"null");return"["+ret.join(",")+"]"}var pairs=[];for(var k in o){var name;var type=typeof k;if(type=="number")name='"'+k+'"';else if(type=="string")name=$.quoteString(k);else continue;if(typeof o[k]=="function")continue;var val=$.toJSON(o[k]);pairs.push(name+":"+val)}return"{"+pairs.join(", ")+"}"}};$.evalJSON=function(src){if(typeof(JSON)=='object'&&JSON.parse)return JSON.parse(src);return eval("("+src+")")};$.secureEvalJSON=function(src){if(typeof(JSON)=='object'&&JSON.parse)return JSON.parse(src);var filtered=src;filtered=filtered.replace(/\\["\\\/bfnrtu]/g,'@');filtered=filtered.replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,']');filtered=filtered.replace(/(?:^|:|,)(?:\s*\[)+/g,'');if(/^[\],:{}\s]*$/.test(filtered))return eval("("+src+")");else throw new SyntaxError("Error parsing JSON, source is not valid.");};$.quoteString=function(string){if(string.match(_escapeable)){return'"'+string.replace(_escapeable,function(a){var c=_meta[a];if(typeof c==='string')return c;c=a.charCodeAt();return'\\u00'+Math.floor(c/16).toString(16)+(c%16).toString(16)})+'"'}return'"'+string+'"'};var _escapeable=/["\\\x00-\x1f\x7f-\x9f]/g;var _meta={'\b':'\\b','\t':'\\t','\n':'\\n','\f':'\\f','\r':'\\r','"':'\\"','\\':'\\\\'}})(jQuery);
 			
 			vkPatch.load.step2();
 		},
@@ -700,6 +701,7 @@ var vkPatch =
 		TYPE_INT: 'int',
 		TYPE_FLOAT: 'float',
 		TYPE_LIST: 'list',			// набор
+		TYPE_OBJECT: 'object',		// объект. нет проверок
 		
 		
 		/*
@@ -825,7 +827,7 @@ var vkPatch =
 							
 						case vkPatch.settings.TYPE_STRING:
 							
-							result_value = value;
+							result_value = '' + value;
 							
 							break;
 							
@@ -872,10 +874,14 @@ var vkPatch =
 					{
 						return vkPatch.settings.TYPE_LIST;	/* набор */
 					}
-					else	/*	строка	*/
+					else if($$.is.string(this.def))
 					{
 						return vkPatch.settings.TYPE_STRING;	/* строка */
-					};
+					}
+					else		/* объект */
+					{
+						return vkPatch.settings.TYPE_OBJECT;
+					}
 				}
 			};
 			
