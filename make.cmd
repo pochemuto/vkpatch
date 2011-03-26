7cc9ab1887886440544d2e68be55a8e234706e55
pochemuto <pochemuto@localhost>
2011-03-26 12:47:56 +0300

    ????? resources ?? chrome extension, ???????? ?????? ?????? ? make.cmd

--- make.cmd
+++ make.cmd
@@ -1,7 +1,9 @@
 @echo off
 echo Hardlinks
-mklink /h 		"chrome extension/vkpatch.user.js" 			vkpatch.user.js
-mklink /h 		"opera extension/vkpatch.user.js" 			vkpatch.user.js
-mklink /j 		"chrome extension/resources" 				resources
+del /f /q "chrome extension\vkpatch.user.js"
+mklink /h 		"chrome extension\vkpatch.user.js" 			vkpatch.user.js
+del "opera extension\vkpatch.user.js"
+mklink /h 		"opera extension\vkpatch.user.js" 			vkpatch.user.js
+rem mklink /j 		"chrome extension/resources" 				resources
 echo Done 
 pause
