8b38e40e953c397311c2bc2280409e1838de1000
pochemuto <pochemuto@localhost>
2011-04-17 02:08:40 +0400

    make firefox extension ?????????? ? ??????? firefox

--- make/make.cmd
+++ make/make.cmd
@@ -1,5 +1,10 @@
 @echo off
 
+rem ======================
+rem ======================
+rem       Hardlinks
+rem ======================
+rem ======================
 echo ====   Hardlinks   ====
 
 cd ..
@@ -37,13 +42,32 @@ rem ==================
 del /f /q "firefox extension\content\vkpatch.user.js"
 mklink /h "firefox extension\content\vkpatch.user.js"		vkpatch.user.js
 
+del /f /q "firefox extension\content\icons\icon_48.png"
+mklink /h "firefox extension\content\icons\icon_48.png"		"resources\icon_48.png"
+
+del /f /q "firefox extension\content\icons\icon_64.png"
+mklink /h "firefox extension\content\icons\icon_64.png"		"resources\icon_64.png"
+
+rem ======================
+rem ======================
+rem       Packaging
+rem ======================
+rem ======================
+
 echo.
 echo ====   Packaging   ====
 rem 7-Zip Command Line Version - http://www.7-zip.org/download.html
+
+rem ==================
+rem       Opera
+rem ==================
 del make\vkpatch-opera.oex
 7za a -tzip make\vkpatch-opera.oex ".\opera extension\*"
 echo Opera extension - done
 
+rem ==================
+rem       Chrome
+rem ==================
 rem %chrome% - путь к chrome.exe
 del make\vkpatch-chrome.crx
 if exist "make\vkpatch-chrome-key.pem" (
@@ -57,6 +81,13 @@ if exist "make\vkpatch-chrome-key.pem" (
 
 move "chrome extension.crx" make\vkpatch-chrome.crx
 echo Chrome extension - done
+
+rem ==================
+rem       Firefox
+rem ==================
+del make\vkpatch-firefox.xpi
+7za a -tzip make\vkpatch-firefox.xpi ".\firefox extension\*"
+echo Firefox extension - done
 
 echo Done 
 cd make
