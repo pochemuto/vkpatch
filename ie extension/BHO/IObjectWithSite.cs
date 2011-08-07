f211dd337983445b93300aad85fe1e8a92899e1f
pochemuto <devnull@localhost>
2011-08-07 21:33:09 +0400

    ie extension

---
+++ ie extension/BHO/IObjectWithSite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace vkpatch
+{
+    [
+    ComVisible(true),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown),
+    Guid("FC4801A3-2BA9-11CF-A229-00AA003D7352")
+    ]
+
+    public interface IObjectWithSite
+    {
+        [PreserveSig]
+        int SetSite([MarshalAs(UnmanagedType.IUnknown)]object site);
+        [PreserveSig]
+        int GetSite(ref Guid guid, out IntPtr ppvSite);
+    }
+}
