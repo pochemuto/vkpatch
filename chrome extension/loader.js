03a8971e5e8aa2381c5938767a004c917c337838
pochemuto <pochemuto@localhost>
2011-12-22 23:25:16 +0400

    Issue 21: ????????? ???????? ? google chrome

--- chrome extension/loader.js
+++ chrome extension/loader.js
@@ -1,16 +1,54 @@
-// Подгружаем на страницу ссылка на скрипт, чтобы обращаться к картинкам
-var url = chrome.extension.getURL('');
-var include = ['settings', 'kikuyutoo'];
+var pluginsFolder = "plugins/";
 
-loadScript(null,'var vkpatchUrl = "' + url+'";');
-
-// подгружаем сам скрипт
-loadScript(chrome.extension.getURL('vkpatch.user.js'));
-
-
-for (var i=0; i<include.length; i++)
+read( getUrl( pluginsFolder + 'list.txt' ), function(list)
 {
-	loadScript( chrome.extension.getURL( 'plugins/' + include[i] + '.js' ) );
+	var pluginsNames = list.content.split(/[\n\r]+/);
+	inject('vkpatch.user.js');
+	for (var i = 0; i < pluginsNames.length; i++) 
+	{
+		inject( pluginsFolder + pluginsNames[i] + '.js');
+	};
+});
+
+function getUrl(path) 
+{
+	return chrome.extension.getURL(path);
+};
+
+function inject(url) 
+{
+	loadScript( getUrl( url ), false, url );
+};
+
+function read(url, callback) 
+{
+	var req = new XMLHttpRequest();
+	req.open('GET', url, true);
+	req.onreadystatechange = function() 
+	{
+		try 
+		{ 
+			if (req.readyState == 4) 
+			{
+				if (req.status == 200) 
+				{
+					callback({
+						content: req.responseText,
+						url: url
+					});
+				}
+				else 
+				{
+					//TODO: обработка ошибок 
+				}
+			}
+		} 
+		catch (e) 
+		{
+		}
+	};
+	
+	req.send(null);
 };
 
 /**
@@ -18,7 +56,7 @@ for (var i=0; i<include.length; i++)
  * @param {scting} src - url к внешнему скрипту
  * @param {string} code - код
  */
-function loadScript(src, code)
+function loadScript(src, code, scriptUrl)
 {
 	var script = document.createElement('script');
 	if (code)
@@ -31,5 +69,9 @@ function loadScript(src, code)
 	}
 	script.type = 'text/javascript';
 	script.charset = 'utf-8';
+	if (scriptUrl) 
+	{
+		script.setAttribute("data-source", scriptUrl); 
+	};
 	window.document.getElementsByTagName('head')[0].appendChild(script);
 };
