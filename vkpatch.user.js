a164a187ed202a2bcce7637f6ed58a935f1cc144
pochemuto <devnull@localhost>
2012-02-11 21:36:19 +0400

    Удалён jQuery.cookie

--- vkpatch.user.js
+++ vkpatch.user.js
@@ -115,11 +115,6 @@ var vkPatch =
 			 * Подключаем необходимые плагины jQuery
 			 */
 		
-			// Cookies
-			// http://plugins.jquery.com/node/1387
-			jQuery.cookie=function(a,b,c){if(typeof b!='undefined'){c=c||{};if(b===null){b='';c=$.extend({},c);c.expires=-1;}var d='';if(c.expires&&(typeof c.expires=='number'||c.expires.toUTCString)){var e;if(typeof c.expires=='number'){e=new Date();e.setTime(e.getTime()+(c.expires*24*60*60*1000));}else{e=c.expires;}d='; expires='+e.toUTCString();}var f=c.path?'; path='+(c.path):'';var g=c.domain?'; domain='+(c.domain):'';var h=c.secure?'; secure':'';document.cookie=[a,'=',encodeURIComponent(b),d,f,g,h].join('');}else{var j=null;if(document.cookie&&document.cookie!=''){var k=document.cookie.split(';');for(var i=0;i<k.length;i++){var l=jQuery.trim(k[i]);if(l.substring(0,a.length+1)==(a+'=')){j=decodeURIComponent(l.substring(a.length+1));break;}}}return j;}};
-			
-
 			/*
 			 * jQuery JSON Plugin
 			 * version: 2.1 (2009-08-14)
